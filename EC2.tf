f32744c25174fe9f329d70bccd1b59385b2125c0
Sergey Bondarenko <sbondar05@gmail.com>
2021-12-16 11:16:55 +0300

    add 2 NGINX

--- EC2.tf
+++ EC2.tf
@@ -86,6 +86,33 @@ egress = [
   }
 }
 
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
 resource "aws_security_group" "allow_elb" {
   name        = "allow_elb"
   description = "Allow ELB inbound traffic"
@@ -184,13 +211,21 @@ resource "aws_iam_role_policy" "read-registry" {
 
 
 resource "aws_instance" "test_instance" {
-  ami           = "ami-05d34d340fb1d89e5"
+  ami           = "ami-05d34d340fb1d89e5"  # Amazon Linux AMI
     instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.allow_elb.id]
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.allow_elb.id, aws_security_group.allow_tls.id]
   subnet_id = aws_subnet.new-public-01.id
   iam_instance_profile = aws_iam_instance_profile.ec2-registry.name
   root_block_device {
     volume_size = 8
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get -y update",
+      "sudo apt-get -y install nginx",
+      "sudo service nginx start",
+    ]
   }
 
   tags = {
@@ -199,13 +234,21 @@ resource "aws_instance" "test_instance" {
 }
 
 resource "aws_instance" "test_instance2" {
-  ami           = "ami-05d34d340fb1d89e5"
+  ami           = "ami-05d34d340fb1d89e5" # Amazon Linux AMI
     instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.allow_elb.id]
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.allow_elb.id, aws_security_group.allow_tls.id]
   subnet_id = aws_subnet.new-public-02.id
   iam_instance_profile = aws_iam_instance_profile.ec2-registry.name
   root_block_device {
     volume_size = 8
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get -y update",
+      "sudo apt-get -y install nginx",
+      "sudo service nginx start",
+    ]
   }
 
   tags = {
--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
+#----------------------------------------------------------
+#
+# AWS 2 NGINX+ALB
+#
+# Made by Sergey Bondarenko
+#
+#----------------------------------------------------------
+
 provider "aws" {
   region = "eu-central-1"
   shared_credentials_file = "/Users/siarhei/.aws/credentials"
 }
-
-
 
 resource "aws_ecr_repository" "python" {
   name                 = "testpython"
